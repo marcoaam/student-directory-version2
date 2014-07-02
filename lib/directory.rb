f1ef62a000b42fb9d8e219f8d431b33984df089d
marcoaam <marco.army@gmail.com>
2014-07-02 17:48:47 +0100

    Third finished student directory

---
+++ lib/directory.rb
@@ -0,0 +1,89 @@
+require 'csv'
+def student_list
+	@students ||= []
+end
+
+def student_profile(name, cohort)
+	return {name: name, cohort: cohort.to_sym} if cohort != nil
+end
+
+def add(student)
+	student_list << student
+end
+
+def names_that_begin_with(letter = "A")
+	student_list.select {|student| student[:name].include?(letter)}
+end
+
+def names_with_less_than_number_of_letters(number)
+	student_list.select {|student| student[:name].length <= number}
+end
+
+def show(student_list)
+	return student_list.each {|student| puts "#{student[:name]}, #{student[:cohort]} cohort"} if student_list != nil
+end
+
+def save_to_file(filename = "students.csv")
+	CSV.open(filename, "wb") do |csv|
+		student_list.each {|student| csv << student_values(student)}
+			#student_list.each {|student|  csv << student.values}
+	end
+end
+
+def student_values(student)
+	student.values
+end
+
+def load_from_file(filename = "students.csv")
+	CSV.foreach(filename) do |row|
+		name, cohort = row[0], row[1]
+		add(student_profile(name,cohort))
+	end
+end
+
+def get_user_input
+	STDIN.gets.chomp
+end
+
+def get_student_name
+	puts "Enter the name of the student"
+	get_user_input
+end
+
+def get_student_cohort
+	puts "Enter the month of the cohort"
+	get_user_input
+end
+
+def print_menu_options
+	puts "Main menu\n1-.Add a student\n2-.Show the list of students\n3-.Save the list to a file\n4-.Load a list from a file\n5-.Show students names that begin with an A\n9-.Exit the program"	
+end
+
+def menu_options
+	print_menu_options
+	menu_selection = get_user_input
+	case menu_selection
+	when "1"
+		add(student_profile(get_student_name,get_student_cohort))
+	when "2"
+		show(student_list)
+	when "3"
+		save_to_file
+	when "4"
+		load_from_file
+	when "5"
+		show(names_that_begin_with("a"))
+	when "6"
+		show(names_with_less_than_number_of_letters(12))
+	when "9"
+		exit
+	else
+		puts "Try again, invalid menu option"
+	end
+end
+
+def interactive_menu
+	loop do
+		menu_options
+	end
+end
---
+++ spec/directory_spec.rb
@@ -0,0 +1,155 @@
+require 'directory'
+
+describe 'Student Directory' do
+		it 'Have an empty array at the beginning' do
+			expect(student_list.empty?).to be true
+		end
+
+		it 'Have a hash for each student' do
+			student_hash = {name: "Marco", cohort: :June}
+			expect(student_profile("Marco", "June")).to eq student_hash
+		end
+
+		it 'Can add a student to the student list' do
+			student_hash = {name: "Marco", cohort: :June}
+			expect(add(student_hash)).to eq [student_hash]
+		end
+
+		it 'Can add two or more students to the student list' do
+			student_hash1 = {name: "Marco", cohort: :June}
+			student_hash2 = {name: "Jean", cohort: :June}
+			add(student_hash1)
+			expect(add(student_hash2)).to eq [student_hash1,student_hash2]
+		end
+
+		it 'Can display the student list to the terminal' do
+			student_list = [{name: "Marco", cohort: :June},{name: "Jean", cohort: :June}]
+			expect(self).to receive(:puts).with("Marco, June cohort")
+			expect(self).to receive(:puts).with("Jean, June cohort")
+			show(student_list)
+		end
+
+		it 'saves a student list to a csv file' do
+			csv = double
+			add({name: "Marco", cohort: :June})
+			expect(CSV).to receive(:open).with(filename = "students.csv", "wb").and_yield(csv)
+			expect(csv).to receive(:<<).with(["Marco", :June])
+			save_to_file(filename)
+		end
+
+		it 'loads a student list from a file called students.csv by default' do
+			row = double
+			student_profile("Marco","June")
+			expect(CSV).to receive(:foreach).with(filename = "students.csv").and_yield(row)
+			expect(row).to receive(:[]).with(0)
+			expect(row).to receive(:[]).with(1)
+			expect(self).to receive(:add)
+			load_from_file(filename)
+		end
+
+		it 'Can get user input' do
+			input = "String"
+			expect(STDIN).to receive(:gets).and_return(input)
+			expect(get_user_input).to eq input
+		end
+
+		it 'Ask the student name' do
+			expect(self).to receive(:puts).with("Enter the name of the student")
+			expect(self).to receive(:get_user_input)
+			get_student_name
+		end
+
+		it 'Ask the student month of the cohort' do
+			expect(self).to receive(:puts).with("Enter the month of the cohort")
+			expect(self).to receive(:get_user_input)
+			get_student_cohort
+		end
+
+		it 'prints a menu with options' do
+			expect(self).to receive(:puts).with("Main menu\n1-.Add a student\n2-.Show the list of students\n3-.Save the list to a file\n4-.Load a list from a file\n5-.Show students names that begin with an A\n9-.Exit the program")
+			print_menu_options
+		end
+
+	context 'have menu options #' do
+		it '1 adds a student' do
+			selection = "1"
+			expect(self).to receive(:print_menu_options)
+			expect(self).to receive(:get_user_input).and_return(selection)
+			expect(self).to receive(:get_student_name)
+			expect(self).to receive(:get_student_cohort)
+			expect(self).to receive(:add)
+			menu_options
+		end
+
+		it '2 show student list' do
+			selection = "2"
+			expect(self).to receive(:print_menu_options)
+			expect(self).to receive(:get_user_input).and_return(selection)
+			expect(self).to receive(:show).with(student_list)
+			menu_options
+		end
+
+		it '3 save the student list into a csv file called students.csv by default' do
+			selection = "3"
+			expect(self).to receive(:print_menu_options)
+			expect(self).to receive(:get_user_input).and_return(selection)
+			expect(self).to receive(:save_to_file)
+			menu_options
+		end
+
+		it '4 load students from a file' do
+			selection = "4"
+			expect(self).to receive(:print_menu_options)
+			expect(self).to receive(:get_user_input).and_return(selection)
+			expect(self).to receive(:load_from_file)
+			menu_options
+		end
+
+		it '5 Print the names that begin with a letter' do
+			selection = "5"
+			expect(self).to receive(:print_menu_options)
+			expect(self).to receive(:get_user_input).and_return(selection)
+			expect(self).to receive(:names_that_begin_with)
+			menu_options
+		end
+
+
+		it '6 Print the names that have less that a number of letters' do
+			selection = "6"
+			expect(self).to receive(:print_menu_options)
+			expect(self).to receive(:get_user_input).and_return(selection)
+			expect(self).to receive(:names_with_less_than_number_of_letters)
+			menu_options
+		end
+
+		it '9 exit from the program' do
+			selection = "9"
+			expect(self).to receive(:print_menu_options)
+			expect(self).to receive(:get_user_input).and_return(selection)
+			expect(self).to receive(:exit)
+			menu_options
+		end
+
+		it 'Invalid selection' do
+			selection = "invalid selection"
+			expect(self).to receive(:print_menu_options)
+			expect(self).to receive(:get_user_input).and_return(selection)
+			expect(self).to receive(:puts).with("Try again, invalid menu option")
+			menu_options
+		end
+	end
+
+	context 'Have Extras' do
+		it 'Shows names that begin with the letter A by default' do
+			add({name: "arco", cohort: :June})
+			add({name: "Luis", cohort: :June})
+			expect(names_that_begin_with("a")).to eq ([{name: "arco", cohort: :June}])
+		end
+
+		it 'Shows students that their names have less that 12 letters' do
+			add({name: "Marcoooooooooooooo", cohort: :June})
+			add({name: "Luis", cohort: :June})
+			expect(names_with_less_than_number_of_letters(12)).to eq ([{name: "Luis", cohort: :June}])
+		end
+	end
+end
